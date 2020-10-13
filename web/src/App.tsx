a996dd82ee2bc04e273fd95cb275b1a02a1c4344
Kevin <kevinsadao@hotmail.com>
2020-10-13 14:50:01 -0300

    Primeiro commit, criado páginas, rotas e implementado mapa com React

---
+++ web/src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './styles/global.css';
+
+import Routes from './routes';
+
+// JSX (JavaScript XML) = forma de incluir html dentro do código XML
+
+// interface TitleProps {
+//   text: string;
+// }
+
+// function Title(props: TitleProps) {
+//   return (
+//     <h1>{props.text}</h1>
+//   )
+// }
+
+// The App() function returns a JSX content (HTML) to be used in index.html
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
