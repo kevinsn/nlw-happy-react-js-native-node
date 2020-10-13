d99ee97c032134f9584e92d0771b3ade549ffabf
Kevin <kevinsadao@hotmail.com>
2020-10-13 20:19:15 -0300

    Abstraindo em controller

---
+++ backend/src/routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import OrphanagesController from './controllers/OrphanagesController';
+
+const routes = Router();
+
+// MVC
+
+// Model
+// Views
+// Controllers
+
+routes.post('/orphanages', OrphanagesController.create);
+
+export default routes;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,13 @@
 import express from 'express';
-import { getRepository } from 'typeorm';
-import Orphanage from './models/Orphanage';
-
 import './database/connection';
+
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(routes);
 
 // Rota = conjunto
 // Recurso = usuário
@@ -23,34 +24,6 @@ app.use(express.json());
 // Query Params: https://locahost:3333/users?serach=diego&page=2
 // Route Params: https://locahost:3333/users/1 (Identificar um recurso)
 // Body: https://locahost:3333/users/1 (Identificar um recurso)
-
-app.post('/orphanages', async (request, response) => {
-    const {
-        name,
-	    latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    } = request.body;
-
-    const orphanagesRepository = getRepository(Orphanage);
-
-    const orphanage = orphanagesRepository.create({
-        name,
-	    latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    });
-    
-    await orphanagesRepository.save(orphanage);
-
-    return response.json({ message: 'hello world' })
-});
 
 app.listen(3333);
 
